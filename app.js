7f527c7a531bcd9dec66a40337919841732e9859
MartinMAPerez <martinmatiasperez2015@gmail.com>
2020-03-26 17:43:44 -0300

    Dark mode button

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+//Cards open-close
+
 let expand = document.getElementById('expandButton')
 let cards = document.querySelectorAll('#cardd')
 let closeButton = document.getElementById('expandButtonClose')
@@ -20,3 +22,36 @@ function cardsShow() {
         tramitTab[i].style.cssText = "display: none"
     }
 }
+
+//Day-Night mode
+
+if (localStorage.getItem('theme')) {
+    var theme = localStorage.getItem('theme');
+    document.body.classList.remove('dark','light');
+    document.body.classList.add(theme);
+  }
+  
+  var switchTheme = document.querySelector('.dark-btn');
+  var toggleLight = document.querySelector('.light-btn');
+  var toggleDark = document.querySelector('.dark-btn');
+  
+  switchTheme.addEventListener('click', function(e) {
+    e.preventDefault();
+  });
+  toggleLight.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    toggleLight.style="display: none";
+    toggleDark.style="display: block";
+    localStorage.setItem('theme', 'light');
+  });
+  toggleDark.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.add('dark');
+    toggleDark.style="display: none";
+    toggleLight.style="display: block";
+    document.body.classList.remove('light');
+    localStorage.setItem('theme', 'dark');
+  });
+  
